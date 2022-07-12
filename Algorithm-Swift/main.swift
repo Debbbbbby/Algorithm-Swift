c6bde7a553750726f82e44532e867a9f5d10ac50
Debby <debby_@kakao.com>
2022-07-12 18:34:30 +0900

    220712

---
+++ Algorithm-Swift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Algorithm-Swift
+//
+//  Created by Debby on 2022/07/12.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Programmers/220712.swift
@@ -0,0 +1,25 @@
+//
+//  220712.swift
+//  Algorithm-Swift
+//
+//  Created by Debby on 2022/07/12.
+//
+
+import Foundation
+
+
+
+// 첫번째 풀이
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+for i in 0..<b {
+    for j in 0..<a {
+        print("*", terminator:"")
+    }
+    print()
+}
+
+// 두번째 풀이
+//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//print(String(repeating: String(repeating: "*", count: n[0])+"\n",count: n[1]))
